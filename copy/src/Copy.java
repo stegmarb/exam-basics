aa49ad11e7a994b0e6f09ae171fd303399ddf029
Balazs Stegmar <stegmarb@gmail.com>
2017-04-18 11:03:08 +0200

    feat(Copy): print out usage of no argument is typed, and handle scenario when only source is typed

--- copy/src/Copy.java
+++ copy/src/Copy.java
@@ -1,12 +1,9 @@
 public class Copy {
   public static void main(String[] args) {
-    // This should be the basic replica of the 'cp' command
-    // If ran from the command line without arguments
-    // It should print out the usage:
-    // copy [source] [destination]
-    // When just one argument is provided print out
-    // No destination provided
-    // When both arguments provided and the source is a file
-    // Read all contents from it and write it to the destination
+    if (args.length == 0) {
+      System.out.println("\n Usage:\n copy [source] [destination]");
+    } else if (args[0].equals("copy") && args.length == 2) {
+      System.out.println("No destination provided!");
+    }
   }
 }
