8e146d7751f465067945e990146cf932af236e91
Balazs Stegmar <stegmarb@gmail.com>
2017-04-18 11:42:39 +0200

    feat(Copy): reads the source file content, creates new file in destinaion derectiory with provided name and writes the content into

--- copy/src/Copy.java
+++ copy/src/Copy.java
@@ -1,9 +1,39 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Copy {
   public static void main(String[] args) {
+    argumentConditions(args);
+  }
+
+  public static void argumentConditions(String[] args) {
     if (args.length == 0) {
       System.out.println("\n Usage:\n copy [source] [destination]");
+    } else if (args[0].equals("copy") && args.length == 1) {
+      System.out.println("No source and destination provided!");
     } else if (args[0].equals("copy") && args.length == 2) {
       System.out.println("No destination provided!");
+    } else if (args[0].equals("copy") && args.length == 3) {
+      copyPasteSourceToDestination(args[1], args[2]);
+    }
+  }
+
+  public static void copyPasteSourceToDestination(String source, String destination) {
+    Path sourcePath = Paths.get(source);
+    Path destinationPath = Paths.get(destination);
+    try {
+      List<String> lines = Files.readAllLines(sourcePath);
+      try {
+        Files.createFile(destinationPath);
+      } catch (IOException e) {
+        System.out.println("Unable to reach destination!");
+      }
+      Files.write(destinationPath, lines);
+    } catch (IOException e) {
+      System.out.println("Unable to read source file!");;
     }
   }
 }
