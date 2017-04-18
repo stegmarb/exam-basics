eb57b3169dc08268e4bcd65f46bcdc159385e99e
Balazs Stegmar <stegmarb@gmail.com>
2017-04-18 12:09:48 +0200

    feat(OddAvg): oddAverage method is ready

--- oddavg/src/OddAvg.java
+++ oddavg/src/OddAvg.java
@@ -1,2 +1,15 @@
+import java.util.List;
+
 public class OddAvg {
+  public int oddAverage(List<Integer> numbers) {
+    int sumOfOddNumbers = 0;
+    int numOfOddNumbers = 0;
+    for (Integer number : numbers) {
+      if (number%2 == 1) {
+        sumOfOddNumbers+=number;
+        numOfOddNumbers++;
+      }
+    }
+    return sumOfOddNumbers/numOfOddNumbers;
+  }
 }
