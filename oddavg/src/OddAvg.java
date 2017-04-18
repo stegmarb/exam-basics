442bf866651d0bc8ed34af6f2e850eedac55b96a
Balazs Stegmar <stegmarb@gmail.com>
2017-04-18 12:58:03 +0200

    test(OddAvg): app is now tewted on list with no odd numbers

--- oddavg/src/OddAvg.java
+++ oddavg/src/OddAvg.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.List;
 
 public class OddAvg {
@@ -6,11 +5,15 @@ public class OddAvg {
     double sumOfOddNumbers = 0;
     double numOfOddNumbers = 0;
     for (Integer number : numbers) {
-      if (number%2 == 1) {
-        sumOfOddNumbers+=number;
+      if (number % 2 == 1) {
+        sumOfOddNumbers += number;
         numOfOddNumbers++;
       }
     }
-    return sumOfOddNumbers/numOfOddNumbers;
+    if (sumOfOddNumbers == 0) {
+      return 0;
+    } else {
+      return sumOfOddNumbers / numOfOddNumbers;
+    }
   }
 }
--- oddavg/test/OddAvgTest.java
+++ oddavg/test/OddAvgTest.java
@@ -1,22 +1,26 @@
 import org.junit.Test;
-
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class OddAvgTest {
-  private static List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
   private List<Integer> emptyList;
 
   @Test
   public void testOddAverage() {
-    double result = OddAvg.oddAverage(numbers);
-    assertTrue(result == 10);
+    List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+    assertTrue(OddAvg.oddAverage(numbers) == 10);
   }
 
   @Test
   public void testNullOddAverage() {
     assertNull(emptyList);
+  }
+
+  @Test
+  public void testIfNoOddNumber() {
+    List<Integer> numbers = Arrays.asList(2,4,6,8,10);
+    assertTrue(OddAvg.oddAverage(numbers) == 0);
   }
 }
